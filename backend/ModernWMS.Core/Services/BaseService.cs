1b203b0c00c3573be8eca37e4fcf41d2a90756f1
李伟 <liwei886252@gmail.com>
2026-08-17 18:23:29 +0800

    清理：移除基础服务的EF残留引用

--- backend/ModernWMS.Core/Services/BaseService.cs
+++ backend/ModernWMS.Core/Services/BaseService.cs
@@ -1,13 +1,5 @@
 ﻿
-using Mapster;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
 using ModernWMS.Core.Models;
-using Microsoft.Extensions.Localization;
 
 namespace ModernWMS.Core.Services
 {
